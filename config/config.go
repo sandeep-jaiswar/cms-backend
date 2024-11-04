6abadc631eca93c6f7ab26dc3fa5aacb2796316b
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-11-04 13:59:34 +0530

    feat: read config from env

--- config/config.go
+++ config/config.go
@@ -25,7 +25,18 @@ type Config struct {
 var AppConfig Config
 
 func LoadConfig() {
-    viper.SetConfigFile("config/config.yaml")
+    viper.SetConfigName("config")
+    viper.SetConfigType("yaml")   // Set the type to yaml
+    viper.AddConfigPath("config")      // Path to look for the config file in
+
+    viper.BindEnv("server.port", "SERVER_PORT")
+    viper.BindEnv("database.host", "DB_HOST")
+    viper.BindEnv("database.port", "DB_PORT")
+    viper.BindEnv("database.user", "DB_USER")
+    viper.BindEnv("database.password", "DB_PASSWORD")
+    viper.BindEnv("database.dbname", "DB_NAME")
+    viper.BindEnv("kafka.brokers", "KAFKA_BROKERS")
+    viper.BindEnv("kafka.topic", "KAFKA_TOPIC")
     if err := viper.ReadInConfig(); err != nil {
         log.Fatalf("Error reading config file: %v", err)
     }
